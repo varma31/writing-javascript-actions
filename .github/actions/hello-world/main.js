5a42bc8c2a4acf6699f30b43da4e94dd16f24e34
varma31 <7084970+varma31@users.noreply.github.com>
2021-09-13 07:20:21 -0500

    allow input in all action files

--- .github/actions/hello-world/main.js
+++ .github/actions/hello-world/main.js
@@ -1,1 +1,14 @@
+const core = require("@actions/core");
+
+const firstGreeting = core.getInput("first-meeting");
+const secondGreeting = core.getInput("second-meeting");
+const thirdGreeting = core.getInput("third-meeting");
+
+console.log(`Hello ${firstGreeting}`);
+console.log(`Hello ${secondGreeting}`);
+
+if (thirdGreeting) {
+    console.log(`Hello ${thirdGreeting}`);
+}
+
 console.log("Hello World");
